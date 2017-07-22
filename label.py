e0dd90fcc8b159b1fb178d95c40b3797da3a5a77
Prashant <gillyprash29@gmail.com>
2017-07-23 01:37:27 +0530

    Added graph,labels and pics

--- label.py
+++ label.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tensorflow as tf
 import os
 import sys
@@ -22,28 +21,3 @@ with tf.Session() as sess:
         result = labels[x]
         score = predict[x]
         print('%s  =  %.5f ' % (result, score))
-=======
-import tensorflow as tf
-import os
-import sys
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-input_layer = 'DecodeJpeg/contents:0'
-output_layer = 'final_result:0'
-image = tf.gfile.FastGFile(sys.argv[1], 'rb').read()
-labels = [line.rstrip() for line in tf.gfile.GFile(sys.argv[2])]
-with tf.gfile.FastGFile(sys.argv[3], 'rb') as f:
-    g_def = tf.GraphDef()
-    g_def.ParseFromString(f.read())
-    tf.import_graph_def(g_def, name='')
-
-with tf.Session() as sess:
-    soft_tensor=sess.graph.get_tensor_by_name(output_layer)
-    predict, = sess.run(soft_tensor, {input_layer: image})
-    top = predict.argsort()[-2:][::-1]
-    for x in top:
-        result = labels[x]
-        score = predict[x]
-        print('%s  =  %.5f ' % (result, score))
->>>>>>> 5cbcb9d09ae6789728fb21fc01e204c52f47e9bb
